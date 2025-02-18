83f3157e5cbdc9acd571dce3937dfc95825efd1b
loma000 <90094687+loma000@users.noreply.github.com>
2025-02-18 18:11:42 +0700

    ss

--- average.cpp
+++ average.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+ 
+ 
+using namespace std;
+ 
 
+int main(int argc,char* argv[]){
+if (argc==1)
+{
+    cout<< "Please input numbers to find average.";
+}else
+{
+   
+float a;
+for (int i = 1; i < argc; i++)
+{
+   a += atof(argv[i]) ;
+}
+cout<<"---------------------------------"<<endl;
+cout << "Average of "<< argc-1 <<" numbers = "<<a/float(argc-1)<<endl;
+cout<<"---------------------------------" ;
+}
+
+
+
+return 0;
+
+}
